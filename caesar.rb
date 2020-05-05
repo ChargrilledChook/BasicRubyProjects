5ab83021a05cf21484a860de4f25f96a93324dac
Dylan Williams <happyfrog578+github@gmail.com>
2020-05-05 11:44:41 +1000

    initial commit, caesar complete

---
+++ caesar.rb
@@ -0,0 +1,18 @@
+def caesar(message, code) 
+  coded = message.bytes.map do |digit|
+    if digit.between?(97, 122) # 97 - 122 is the range for lowercase ascii characters
+      (digit - 97 + code) % 26 + 97
+    elsif digit.between?(65, 90) #65 - 90 is the range for uppercase
+        (digit - 65 + code) % 26 + 65
+    else
+      digit
+    end
+  end
+  decoded = coded.map{|digit| digit.chr}.join
+end
+
+pasta = 'Yum, pasta!!'
+
+puts caesar(pasta, -5000)
+
+
