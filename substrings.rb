43363b4be8d5288e9e0a828a1cacf33611b6c40b
Dylan Williams <happyfrog578+github@gmail.com>
2020-05-09 00:41:49 +1000

    solve substrings

---
+++ substrings.rb
@@ -0,0 +1,20 @@
+def sub_strings(string, dictionary)
+
+  string_array = string.downcase.split
+
+  match_table = string_array.reduce(Hash.new(0)) do |hash, input|
+    dictionary.each do |word| 
+     if input.include?(word)
+      hash[word] += 1
+     end
+    end
+    hash
+  end
+end
+
+# Test inputs from spec
+dict = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+input_test = "Howdy partner, sit down! How's it going?"
+
+# Will return true if my output matches spec output
+p sub_strings(input_test, dict) == { "down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1 }
